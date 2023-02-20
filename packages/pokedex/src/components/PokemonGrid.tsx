6751782d992cf896b75c5ecb3708a8e2bcd16400
Terryb <terrydagbartels@gmail.com>
2023-02-20 13:42:18 +0000

    pokemon grid setup

---
+++ packages/pokedex/src/components/PokemonGrid.tsx
@@ -0,0 +1,35 @@
+
+import React, { useState, useEffect } from 'react';
+import { useRouter } from "next/router";
+import { useDispatch, useSelector } from 'react-redux'
+import { DataGrid, GridCellParams } from '@mui/x-data-grid';
+import { fetchPokemon, setSelectedPokemon, selectPokemon } from 'store/PokemonSlice'
+
+const PokemonGrid = () => {
+        
+    const dispatch = useDispatch();
+    const pokemons = useSelector(selectPokemon);
+    const router = useRouter();
+  
+    useEffect(() => {
+      dispatch(fetchPokemon());
+    }, [dispatch]);
+  
+    const handleRowClick = (params: GridCellParams) => {
+        dispatch(setSelectedPokemon(params.row));
+        router.push("/pokemon/[name]", `/pokemon/${params.row.name}`);
+      };
+    
+      return (
+        <div style={{ height: "500px", width: "100%" }}>
+          <DataGrid rows={pokemons} columns={columns} pageSize={10} onRowClick={handleRowClick} />
+        </div>
+      );
+    }
+    
+    const columns = [
+      { field: "name", headerName: "Name", width: 200 },
+      { field: "url", headerName: "URL", width: 300 },
+    ];
+
+export default PokemonGrid;
