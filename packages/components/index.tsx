4b1eb36136945e84e0353a12917bfd09c1eb4fe5
Terryb <terrydagbartels@gmail.com>
2023-02-20 13:31:11 +0000

    add component entry point

---
+++ packages/components/index.tsx
@@ -0,0 +1,5 @@
+import PokemonDetails from "./src/PokemonCard";
+import { Button } from './src/Button'
+import {ErrorComponent} from './src/Error'
+
+export { PokemonDetails, Button, ErrorComponent }
